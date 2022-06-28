23c5ae7cd18b581b4d38d12f0c832dd8560f3c68
Aan Evian Nanda <71364724+aaneviannanda@users.noreply.github.com>
2022-06-28 15:04:05 +0700

    fixing typo

--- resources/views/layout/admin/footer.blade.php
+++ resources/views/layout/admin/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white fixed-bottom position-relative">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; 2021 Rafa Cake & Bakery</span>
+            <span>Copyright &copy; 2022 Rafa Cake & Bakery</span>
         </div>
     </div>
 </footer>
